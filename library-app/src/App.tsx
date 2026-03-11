543ef052035e80fc165e095beca8468ef25d53ff
masdefry <muhammaddefryan@gmail.com>
2026-03-11 16:38:36 +0700

    pagination

--- library-app/src/App.tsx
+++ library-app/src/App.tsx
@@ -6,11 +6,13 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 export default function App() {
   const [members, setMembers] = useState<any[]>([]);
+  const [totalPage, setTotalPage] = useState<number>(0);
 
   const onGetMembers = async () => {
     try {
       const response = await axios.get('http://localhost:8000/api/members');
       setMembers(response?.data?.data?.members);
+      setTotalPage(response?.data?.data?.totalPage);
     } catch (error) {
       console.log(error);
     }
@@ -66,7 +68,9 @@ export default function App() {
                 return (
                   <tr key={index}>
                     <th>{member?.id}</th>
-                    <td>{member?.firstName} {member?.lastName}</td>
+                    <td>
+                      {member?.firstName} {member?.lastName}
+                    </td>
                     <td>
                       <span className='py-1 px-5 bg-green-200 rounded-full'>
                         {member?.status}
@@ -81,6 +85,20 @@ export default function App() {
               })}
             </tbody>
           </table>
+          <div className='p-5 flex justify-end items-center gap-3'>
+            <p className='font-bold text-md'>Page:</p>
+            <div className='join'>
+              {Array.from({ length: totalPage }, (v, i) => i + 1)?.map(
+                (page: number, index: number) => {
+                  return (
+                    <button key={index} className='join-item btn btn-xs'>
+                      {page}
+                    </button>
+                  );
+                },
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </main>
